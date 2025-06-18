8d65f465204d6bf2ca27be1d95c67384a15a8e8d
Nisha Singh <nishasinghnus@gmail.com>
2025-06-18 12:56:42 +0530

    Update Recommendations.jsx

--- client/src/pages/Recommendations.jsx
+++ client/src/pages/Recommendations.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { startTransition, useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import "./Recommendations.css";
 
